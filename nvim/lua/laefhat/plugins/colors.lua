dea33794be745772f3c95df454f9439491a9b929
Khoa Nguyen <khoanguyenx@icloud.com>
2025-07-29 08:53:32 +0700

    cleanup

--- nvim/lua/laefhat/plugins/colors.lua
+++
@@ -1,4 +0,0 @@
-require("rose-pine").setup({ dark_variant = "moon", styles = { italic = false } })
-vim.cmd.colorscheme("rose-pine")
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
--- nvim/lua/laefhat/plugins/init.lua
+++ nvim/lua/laefhat/plugins/init.lua
@@ -2,11 +2,8 @@ vim.pack.add({
     { src = "https://github.com/tpope/vim-fugitive" },
     { src = "https://github.com/tpope/vim-surround" },
     { src = "https://github.com/mhinz/vim-signify" },
-    { src = "https://github.com/rose-pine/neovim", name = "rose-pine" },
     { src = "https://github.com/junegunn/fzf" },
     { src = "https://github.com/junegunn/fzf.vim" },
     { src = "https://github.com/ziglang/zig.vim" },
 })
-
-require("laefhat.plugins.colors")
 require("laefhat.plugins.fuzzy")
