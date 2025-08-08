0bdb45832d6cbe4f20e59cc2bc920744235f205f
Khoa Nguyen <khoanguyenx@icloud.com>
2025-08-08 12:04:38 +0700

    treesitter textobjects experimental

--- nvim/lua/kdnguyen/plugins/init.lua
+++ nvim/lua/kdnguyen/plugins/init.lua
@@ -4,6 +4,7 @@ vim.pack.add({
     { src = "https://github.com/tpope/vim-surround" },
     { src = "https://github.com/mhinz/vim-signify" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter-textobjects" },
 })
 -- prefer separate config per plugin
 require("kdnguyen.plugins.treesitter")
--- nvim/lua/kdnguyen/plugins/treesitter.lua
+++ nvim/lua/kdnguyen/plugins/treesitter.lua
@@ -16,7 +16,21 @@ require("nvim-treesitter.configs").setup({
     -- parser_install_dir = "/some/path/to/store/parsers", -- remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
     highlight = {
-        enable = false,
+        enable = true,
+
+        -- note: these are the names of the parsers and not the filetype. (for example if you want to
+        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+        -- the name of the parser)
+        -- list of language that will be disabled
+        disable = { },
+        -- or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+        disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 kb
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+                return true
+            end
+        end,
 
         -- setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- set this to `true` if you depend on 'syntax' being enabled (like for indentation).
@@ -26,5 +40,47 @@ require("nvim-treesitter.configs").setup({
     },
     indent = {
         enable = true
+    },
+    textobjects = {
+        select = {
+            enable = true,
+
+            -- automatically jump forward to textobj, similar to targets.vim
+            lookahead = true,
+
+            keymaps = {
+                -- you can use the capture groups defined in textobjects.scm
+                ["af"] = "@function.outer",
+                ["if"] = "@function.inner",
+                ["ac"] = "@class.outer",
+                -- you can optionally set descriptions to the mappings (used in the desc parameter of
+                -- nvim_buf_set_keymap) which plugins like which-key display
+                ["ic"] = { query = "@class.inner", desc = "select inner part of a class region" },
+                -- you can also use captures from other query groups like `locals.scm`
+                ["as"] = { query = "@local.scope", query_group = "locals", desc = "select language scope" },
+            },
+            -- you can choose the select mode (default is charwise 'v')
+            --
+            -- can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * method: eg 'v' or 'o'
+            -- and should return the mode ('v', 'v', or '<c-v>') or a table
+            -- mapping query_strings to modes.
+            selection_modes = {
+                ['@parameter.outer'] = 'v', -- charwise
+                ['@function.outer'] = 'v', -- linewise
+                ['@class.outer'] = '<c-v>', -- blockwise
+            },
+            -- if you set this to `true` (default is `false`) then any textobject is
+            -- extended to include preceding or succeeding whitespace. succeeding
+            -- whitespace has priority in order to act similarly to eg the built-in
+            -- `ap`.
+            --
+            -- can also be a function which gets passed a table with the keys
+            -- * query_string: eg '@function.inner'
+            -- * selection_mode: eg 'v'
+            -- and should return true or false
+            include_surrounding_whitespace = false,
+        },
     },
 })
